35bb20cc0450aa89dffa2c186860cf1eb3a3685e
HectorHQ <109182400+HectorHQ@users.noreply.github.com>
2024-02-29 12:17:10 -0600

    Update main.py

--- main.py
+++ main.py
@@ -200,7 +200,7 @@ if __name__ == "__main__":
 
     with trans_1:
         st.text('What is the transaction you are performing?')
-        selection = st.selectbox('Transactions:',options=['Payments_Applications',index=None,'Upload_File'],placeholder='Select an Option')
+        selection = st.selectbox('Transactions:',options=['Payments_Applications','Upload_File'],index=None,placeholder='Select an Option')
 
     if selection == None:
         pass
